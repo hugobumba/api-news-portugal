e6b8677fa829b52434e7f3806dbf7d5fb5dfbf06
Hugo <40358@alunos.upt.pt>
2024-10-03 12:49:06 +0100

    Público source added

--- scrapers/scrapePublico.js
+++ scrapers/scrapePublico.js
@@ -8,16 +8,17 @@ async function scrapePublico() {
 	
 	const articleInfo = [];
 
-	$('#main-content > section.block.block--bf-widget.block--bf-widget-9.editorial-content.editorial-content.new-container > div > div.cs--1.cn--16.mdmax-cs--1.mdmax-cn--16 > div:nth-child(1) > div.cs--1.cn--12.mdmax-cs--1.mdmax-cn--12.md-r-bdr > article.article.article--level-2.article--media-66.tone--news.article--is-unique > div').each((index, element) => {
+	// Corrigido o seletor para buscar os artigos corretamente
+	$('article.article--level-2.article--media-66.tone--news.article--is-unique.article--sublinks.related-3').each((index, element) => {
 		const sourceName = "Público";
 		const sourceUrl = url;
 		const title = $(element).find('.article__title a').text().trim();
 		const desc = $(element).find('.article__lead').text().trim();
-		const img = $(element).find('picture source[type="image/jpeg"]').attr('srcset').split(' ')[0];
+		const img = $(element).find('picture source[type="image/jpeg"]').attr('srcset')?.split(' ')[0];
 		const link = url + $(element).find('.article__title a').attr('href');
 		//console.log({ sourceName, sourceUrl, title, desc, img, link });
 
-        articleInfo.push({ sourceName, sourceUrl, title, desc, img, link });
+		articleInfo.push({ sourceName, sourceUrl, title, desc, img, link });
 	});
 
 	return articleInfo;

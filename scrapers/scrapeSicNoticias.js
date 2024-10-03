826ec10f376e67becfeb862f12826f6f61d9ed4d
Hugo <40358@alunos.upt.pt>
2024-10-03 08:50:18 +0100

    Titles correction

--- scrapers/scrapeSicNoticias.js
+++ scrapers/scrapeSicNoticias.js
@@ -11,7 +11,7 @@ async function scrapeSicNoticias() {
 	$('#root > main > section:nth-child(6) > div > div > div > div > div > ul.list-articles.item-count-1.featured-articles > li > .teaser-article > div').each((index, element) => {
 		const sourceName = "SIC Notícias";
 		const sourceUrl = url;
-		const title = $(element).find('.teaser-article-inner h2.title a').text().trim();
+		const title = $(element).find('.teaser-article-inner h2.title a').text().trim().replace('Em direto/ ', '');
 		const desc = $(element).find('.lead a').text().trim();
 		const img = $(element).find('img').attr('src');
 		const link = url + $(element).find('.title a').attr('href');
